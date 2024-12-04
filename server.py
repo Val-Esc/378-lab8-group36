69ccadc916a97212f0076309643c994c4df18d83
Valerie Escalante <valerie.escalante01@student.csulb.edu>
2024-12-04 17:24:44 +0000

    added query 1

--- server.py
+++ server.py
@@ -1,7 +1,42 @@
 import socket
+from pymongo.mongo_client import MongoClient
+from datetime import datetime, timedelta
+
+
+def query1(collection):
+    moistureSensor_readings = []
+    query = {
+        #identify kitchen fridge using device ID
+        "payload.parent_asset_uid": "yau-1te-5ms-zt3",
+        #use data from past 3 hours
+        "time": {"$gte": datetime.now() - timedelta(hours=3)}
+    }
+    projection = {
+        #include data from moisture meter sensor only
+        "payload.moistureMeter-fridge1": 1,
+        "_id": 0
+    }
+    #run the above query
+    readings = collection.find(query, projection)
+    #append the sensor readings into an array
+    for reading in readings:
+        moistureSensor_readings.append(float(reading['payload']['moistureMeter-fridge1']))
+
+    average = 0
+    if 0 < len(moistureSensor_readings):
+        for i in moistureSensor_readings:
+            average = average + i
+        average = average / len(moistureSensor_readings)
+    else:
+        average = 0
+    return average
+
+def query2():
+
+def query3():
+
 
 def main():
-    from pymongo.mongo_client import MongoClient
 
     uri = "mongodb+srv://val123:val123@cluster0.pvx8w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     
@@ -14,10 +49,13 @@ def main():
         print("Pinged your deployment. You successfully connected to MongoDB database End-to-End-IoT!")
     except Exception as e:
         print(e)
+    
+    #access collection
+    collection = db.loTFrig_virtual
 
     
     # Prompt the user to enter the IP address and port number
-    ipaddress = input("Enter the IP address to bind to (e.g., 127.0.0.1 or press Enter for 0.0.0.0): ") or '0.0.0.0'
+    ipaddress = input("Enter the IP address to bind to (e.g., 127.0.0.1 or press Enter for 0.0.0.0): ") 
     port = int(input("Enter the port number to bind to (e.g., 12345): "))
 
     # Create a server socket
@@ -45,13 +83,18 @@ def main():
                 break
 
             # Decode and print received data from client
-            received_message = data.decode()
+            query_choice = data.decode()
             print(f"Received from client: {received_message}")
+            
+            if query_choice == 1:
+                response = query1(collection)
+            elif query_choice == 2:
+                response = query2(collection)
+            elif query_choice == 3:
+                response = query3(collection)
 
-            # Convert the received message to uppercase
-            response = received_message.upper()
 
-            # Respond back to the client with the uppercase message
+            # Respond back to the client with query result
             incoming_socket.sendall(response.encode('utf-8'))
 
     except socket.error as e:
