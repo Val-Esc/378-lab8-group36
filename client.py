3b32e016a291f039cda8e4404898e53d955d0c26
Valerie Escalante <valerie.escalante01@student.csulb.edu>
2024-12-04 21:20:44 +0000

    cleaned client file

--- client.py
+++ client.py
@@ -22,7 +22,7 @@ success = 'n'
 while success == 'n':
   try:
     serverIP = input("Enter server IP address: ").strip()  # prompt user for server IP address
-    serverPort = int(input("Enter server port number: ")).strip()  # prompt user for server port number
+    serverPort = int(input("Enter server port number: "))  # prompt user for server port number
 
     myTCPSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # establish TCP socket
     myTCPSocket.connect((serverIP, serverPort)) #attempt to connect TCP socket with server Ip address and port number
@@ -39,11 +39,10 @@ while continue_Client == 'y':
   myTCPSocket.send(query.to_bytes(1, byteorder='big')) #send pick as an integer to server
 
   serverResponse = myTCPSocket.recv(1024).decode('utf-8') #recieve server response as letters
-  print("Server response: ", serverResponse) #display server capital message response
+  print("Result of your query: ", serverResponse) #display server capital message response
 
   continue_Client = input("Would you like to process another query? (y/n):") #ask to continue with another query
-  if continue_Client == 'y':
-    query = query_options()
+  
   
 myTCPSocket.close()
 
